a48cdfa822d7c1f02dec19fdc9f1487d401e8a5b
Navya <88023981+navysai1419@users.noreply.github.com>
2023-12-27 18:26:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let audioElements = [];
+
+function playSound(soundFile) {
+    stopAllSounds();
+
+    const audio = new Audio(`sounds/${soundFile}`);
+    audio.play();
+    audioElements.push(audio);
+}
+
+function stopAllSounds() {
+    audioElements.forEach(audio => audio.pause());
+    audioElements = [];
+}
